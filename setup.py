0f09d6190721dea53e731b24b4dba9591525c4fe
Andres Romero <me@afromero.co>
2025-07-18 12:22:20 +0200

    update setup to find torch

--- setup.py
+++ setup.py
@@ -1,8 +1,19 @@
 from setuptools import setup
 import os
+import torch
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
+
+# Get PyTorch library directory
+torch_lib_dir = os.path.join(os.path.dirname(torch.__file__), 'lib')
+rpath_flag = f"-Wl,-rpath,{torch_lib_dir}"
+
 ROOT = os.path.dirname(os.path.abspath(__file__))
+extra_compile_args = {
+    # "cores": ["j8"],
+    "cxx": ["-O2"],
+    "nvcc": ["-O2"],
+}
 
 setup(
     name="lietorch",
@@ -14,15 +25,15 @@ setup(
         CUDAExtension("lietorch_backends", 
             include_dirs=[
                 os.path.join(ROOT, "lietorch/include"), 
-                os.path.join(ROOT, "eigen")],
+                os.path.join(ROOT, "eigen")
+            ],
             sources=[
                 "lietorch/src/lietorch.cpp", 
                 "lietorch/src/lietorch_gpu.cu",
-                "lietorch/src/lietorch_cpu.cpp"],
-            extra_compile_args={
-                "cxx": ["-O2"], 
-                "nvcc": ["-O2"],
-            }),
+                "lietorch/src/lietorch_cpu.cpp"
+            ],
+            extra_compile_args=extra_compile_args,
+            extra_link_args=[rpath_flag]),
 
         CUDAExtension("lietorch_extras", 
             sources=[
@@ -33,10 +44,8 @@ setup(
                 "lietorch/extras/se3_solver.cu",
                 "lietorch/extras/extras.cpp",
             ],
-            extra_compile_args={
-                "cxx": ["-O2"], 
-                "nvcc": ["-O2"],
-            }),
+            extra_compile_args=extra_compile_args,
+            extra_link_args=[rpath_flag]),
     ],
     cmdclass={ "build_ext": BuildExtension }
 )
